80983c55c2186c38e7919fa9bfbde4cc6d1fcaaa
JamesCOrr <thororr@gmail.com>
2016-04-03 20:00:07 -0700

    Adding about to urlmappings.

--- grails-app/controllers/geoforward/UrlMappings.groovy
+++ grails-app/controllers/geoforward/UrlMappings.groovy
@@ -8,7 +8,7 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-
+        "/about"(view: "/about")
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
